b410292a97eee71f28d7fcbb251c72be0691958a
DVSB <vzcrsa12@googlemail.com>
2014-03-08 18:38:34 +0100

    Revert "No"
    This reverts commit 72511e4cc762e04e9ae1098de3567f74a628de65.

--- modules/admin/bandwidth_throttle/modinfo.zp.php
+++
@@ -1,6 +0,0 @@
-<?php
-$thismod['title'] = 'Bandwidth Throttle';
-$thismod['icon'] = 'icon.gif';
-$thismod['developer'] = 'RusTus';
-$thismod['developersite'] = 'http://http://forums.zpanel.co.uk/';
-?>
